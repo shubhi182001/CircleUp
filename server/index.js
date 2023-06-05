0dcaf12b792119c856b2ec0b4df770630298e8a7
shubhi182001 <kediashubhi@gmail.com>
2023-06-05 12:59:17 +0530

    images server side

--- server/index.js
+++ server/index.js
@@ -7,19 +7,40 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-
+const multer = require("multer");
+const path = require("path");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true}).then( () => {
     console.log("Connected to mongo")
 }).catch((e) => {
-    console.log(e) 
+    console.log(e)
 });
+
+app.use("/images", express.static(path.join(__dirname  , "public/images")));
 
 //middlewares:
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, "public/images")
+    },
+    filename:(req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({storage: storage});
+app.post("/api/upload" , upload.single("file"), (req, res) => {
+    try{
+        return res.status(200).json("File Uploaded Successfully");
+    }catch(e) {
+        console.log(e);
+    }
+})
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
